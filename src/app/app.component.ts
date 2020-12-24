f57698d578d118f217213be48d2db183ffedeabe
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2020-12-24 10:26:21 -0300

    GETing Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -24,9 +26,17 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(`https://ng-complete-guide-9de3d-default-rtdb.firebaseio.com/posts.json`)
+    .subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 }
