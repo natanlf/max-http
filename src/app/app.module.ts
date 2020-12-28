e71d0ce6942e7ddf70a95babac89b7840d009f51
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2020-12-28 14:24:03 -0300

    Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [
+  providers: [ //a ordem dos interceptors afeta quem será executado primeiro
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptorService,
+      multi: true
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
-      multi: true //para poder ter multiplos interceptors
-    } ],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,24 +1,13 @@
-import { tap } from 'rxjs/operators';
-import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Request is on its way'); //request é imutável, por isso devemos clonar para modificar
-    console.log(req.url);
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz') //deixa o header antigo e adiciona novos
     });
-    return next.handle(modifiedRequest).pipe(
-      tap(event => {
-        console.log(event);
-        if(event.type === HttpEventType.Response) {
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifiedRequest);
   }
 
 }
