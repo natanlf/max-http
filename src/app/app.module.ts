1a2cc3d244beb7871cb47e52eb84b91c22a17b3a
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2020-12-28 13:55:19 -0300

    Introducing Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { AuthInterceptorService } from './auth-interceptor.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true //para poder ter multiplos interceptors
+    } ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
